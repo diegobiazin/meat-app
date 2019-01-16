9dddd0a4e97517a34877bf88ed498413827869bc
Diego Biazin <diego_biazin@hotmail.com>
2019-01-16 20:45:59 -0200

    Aplicando eventos no componente do carrinho de compras

--- src/app/restaurants-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurants-detail/shopping-cart/shopping-cart.component.ts
@@ -16,6 +16,17 @@ export class ShoppingCartComponent implements OnInit {
     return this.shoppingCartService.items
   }
 
+  clear() {
+    this.shoppingCartService.clear()
+  }
+
+  removeItem(item: any) {
+    this.shoppingCartService.removeItem(item)
+  }
+
+  addItem(item: any) {
+    this.shoppingCartService.addItem(item)
+  }
 
   total(): number {
     return this.shoppingCartService.total()
